920885513bd908c9a43062c16ac68771cec75e27
Anto Pezzola <antopezzola@gmail.com>
2022-09-24 00:00:40 -0300

    tp4 casi terminado

--- tp-4/tp-4.hs
+++ tp-4/tp-4.hs
@@ -61,3 +61,204 @@ cantCapasPorPizza :: [Pizza] -> [(Int, Pizza)]
 cantCapasPorPizza  []     = []
 cantCapasPorPizza  (p:ps) =  (cantidadDeCapas p,p) : cantCapasPorPizza ps
                                                -- devuelvo el int y la pizza
+                                               
+data Dir = Izq | Der
+    deriving Show
+data Objeto = Tesoro | Chatarra
+    deriving Show
+data Cofre = Cofre [Objeto]
+    deriving Show
+data Mapa = Fin Cofre | Bifurcacion Cofre Mapa Mapa
+    deriving Show
+
+cofre1 = Cofre [Chatarra, Chatarra, Tesoro] 
+
+cofre2 = Cofre [Chatarra, Chatarra, Tesoro]
+
+cofre3 = Cofre [Tesoro,Chatarra, Chatarra, Chatarra, Tesoro]
+
+cofre5 = Cofre []
+
+mapa1 = Fin cofre1
+mapa2 = Fin cofre2
+
+mapa3 = Bifurcacion cofre3 mapa1 mapa2
+
+mapa4 = Bifurcacion cofre5 mapa1 mapa3
+
+
+dir = [Der,Der,Izq]
+
+hayTesoro :: Mapa -> Bool
+hayTesoro (Fin c) = tieneTesoro c 
+hayTesoro (Bifurcacion c mi md ) = tieneTesoro c || hayTesoro mi || hayTesoro md    
+
+tieneTesoro :: Cofre -> Bool
+tieneTesoro (Cofre os ) = elObjetoTieneTesoro os 
+
+elObjetoTieneTesoro :: [Objeto] -> Bool
+elObjetoTieneTesoro [] = False 
+elObjetoTieneTesoro (o:os) = esTesoro o || elObjetoTieneTesoro os
+
+esTesoro :: Objeto -> Bool
+esTesoro  Tesoro = True
+esTesoro   _     = False
+
+hayTesoroEn :: [Dir] -> Mapa -> Bool
+hayTesoroEn [] (Bifurcacion c mi md ) = False
+hayTesoroEn _ (Fin c) = tieneTesoro c 
+hayTesoroEn (d:ds) (Bifurcacion c mi md) = if (esIzquierda d) 
+                                        then  hayTesoroEn ds mi
+                                        else hayTesoroEn ds md 
+
+esIzquierda :: Dir -> Bool
+esIzquierda  Izq = True
+esIzquierda   _ = False 
+
+caminoAlTesoro :: Mapa -> [Dir]
+-- PRECOND : existe un tesoro y es único
+caminoAlTesoro (Fin c)               = [] 
+caminoAlTesoro (Bifurcacion c mi md) = 
+
+
+data Componente = LanzaTorpedos | Motor Int | Almacen [Barril]
+    deriving Show
+data Barril = Comida | Oxigeno | Torpedo | Combustible
+        deriving Show
+data Sector = S SectorId [Componente] [Tripulante]
+        deriving Show
+
+type SectorId = String
+type Tripulante = String
+
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+
+data Nave = N (Tree Sector)
+--------------
+sectores :: Nave -> [SectorId]
+sectores (n t) = losSectoresDe t 
+
+
+losSectoresDe :: Tree Sector  -> [SectorId]
+losSectoresDe EmptyT = 
+losSectoresDe (NodeT s t1 t2) = idSector s : losSectoresDe t1 ++ losSectoresDe  t2
+
+idSector :: Sector -> SectorId
+idSector (S id _ _ )  = id 
+
+-----------
+
+poderDePropulsion :: Nave -> Int
+poderDePropulsion (n t) = propulsion t
+
+propulsion :: Tree Sector -> Int
+propulsion EmptyT = 0
+propulsion (NodeT s t1 t2) = compontnesSector s + propulsion t1 + propulsion t2
+
+compontnesSector :: Sector -> Int
+compontnesSector (S _ comp _ ) = compSectorTotal comp
+
+compSectorTotal :: [Componente] -> Int
+compSectorTotal [] = 
+compontnesSector (c:cs) = propulsionC c + compSectorTotal cs
+
+propulsionC :: Componente -> Int
+propulsionC (Motor n) = n 
+propulsionC _         = 0
+--------------
+
+barriles :: Nave -> [Barril]
+-- Prop: devuelve todos los barriles de la nave
+barriles (n t) = barrilesDeLaNave t
+
+barrilesDeLaNave :: Tree Sector -> [Barril] 
+barrilesDeLaNave EmptyT = []
+barrilesDeLaNave (NodeT s t1 t2) =  losBarrilesDelSector s ++ barrilesDeLaNave t1  ++ barrilesDeLaNave t2 
+
+losBarrilesDelSector :: Sector -> [Barril] 
+losBarrilesDelSector (S _ comp _ ) = losBarriles comp
+
+losBarriles :: [Componente] -> [Barril]
+losBarriles [] = []
+losBarriles (c:cs) = barrilesEnComponente c ++ losBarriles cs
+
+barrilesEnComponente :: Componente -> [Barril]
+barrilesEnComponente (Almacen bs) = bs
+
+-------------
+
+agregarASector :: [Componente] -> SectorId -> Nave -> Nave
+
+-------------
+
+sectoresAsignados :: Tripulante -> Nave -> [SectorId]
+sectoresAsignados t (n tr) = losSectoresConTripulanteEnNave t tr
+
+losSectoresConTripulanteEnNave :: Tripulante -> Sector -> [SectorId]
+losSectoresConTripulanteEnNave t EmptyT = []
+losSectoresConTripulanteEnNave t (NodeT s s1 s2) = if sectorConTripulante s t 
+                              then idSector s : losSectoresConTripulanteEnNave t s1 ++ losSectoresConTripulanteEnNave t s2
+                              else losSectoresConTripulanteEnNave t s1 ++ losSectoresConTripulanteEnNave t s2
+
+sectorConTripulante :: Sector -> Tripulante -> Bool
+sectorConTripulante (S _ _ tripu ) t =  estaElTripulante t tripu
+
+estaElTripulante :: Tripulante -> [Tripulante] -> Bool
+estaElTripulante tri (t:ts) = esElMismoTripulante tri t || estaElTripulante tri ts
+
+esElMismoTripulante :: Tripulante -> Tripulante -> Bool
+esElMismoTripulante tri t = tri == t
+
+------------
+
+tripulantes :: Nave -> [Tripulante]
+tripulantes (n t) = tripulantesSinRepetir(losTripulantesDeLaNave t)
+
+losTripulantesDeLaNave :: Tree Sector -> [Tripulante]
+losTripulantesDeLaNave EmptyT = []
+losTripulantesDeLaNave (NodeT s t1 t2) = losTripulantes s ++  losTripulantesDeLaNave t1  ++ losTripulantesDeLaNave t2 
+
+losTripulantes :: Sector -> [Tripulante]
+losTripulantes  (S _ _ tripu ) = tripu
+
+tripulantesSinRepetir :: [Tripulante] -> [Tripulante]
+tripulantesSinRepetir []
+tripulantesSinRepetir (t:ts) = if tripulanteRepetidos t ts 
+                               then tripulantesSinRepetir ts
+                               else t : tripulantesSinRepetir ts
+
+tripulanteRepetidos :: a -> [a] -> Bool
+tripulanteRepetidos y xs = apariciones y xs > 1
+
+apariciones :: Eq a => a -> [a] -> Int
+apariciones  a []  = 0 
+apariciones a (x:xs)  =  unoSi( a == x ) + apariciones a xs
+
+-------------
+
+type Presa      = String -- nombre de presa
+type Territorio = String -- nombre de territorio
+type Nombre     = String -- nombre de lobo
+data Lobo   = Cazador Nombre [Presa] Lobo Lobo Lobo  | Explorador Nombre [Territorio] Lobo Lobo | Cria Nombre
+                deriving Show
+data Manada = M Lobo
+                deriving Show
+
+
+buenaCaza :: Manada -> Bool
+buenaCaza m = cantidadDeAlimento m > cantidadDeCrias m
+
+cantidadDeAlimento :: Manada -> Int
+cantidadDeAlimento (M lobo) = cantidadDeAlimentoL lobo
+
+cantidadDeAlimentoL :: Lobo -> Int
+cantidadDeAlimentoL (Cazador _ presas l1 l2 l3) = alimentoEn presas
+                                                + cantidadDeAlimentoL l1
+                                                + cantidadDeAlimentoL l2
+                                                + cantidadDeAlimentoL l3
+cantidadDeAlimentoL (Explorador _ _ l1 l2)      = cantidadDeAlimentoL l1
+                                                + cantidadDeAlimentoL l2
+cantidadDeAlimentoL (Cria _)                    = 0
+
+alimentoEn :: [Presa] -> Int
+alimentoEn ps = length ps
